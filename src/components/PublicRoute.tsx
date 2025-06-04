f31fac8624fdbf68b6572ca31be17aa31b40765a
Khushali Trivedi <trivedikhushali47@gmail.com>
2025-06-04 11:27:17 +0530

    change

--- src/components/PublicRoute.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-
-const PublicRoute: React.FC = () => {
-  const token = localStorage.getItem('token');
-  return token ? <Navigate to="/admin" /> : <Outlet />;
-};
-
-export default PublicRoute;
